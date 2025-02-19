8d577bf5ee23509df7b40ec74db82cebb3f805d1
Daarick <e.danielgrz10@gmail.com>
2025-02-18 22:29:59 -0600

    Version 1

---
+++ classifier.py
@@ -0,0 +1,38 @@
+c1 = ((1,2), (2,3), (0,2), (1,1), (2,2))
+c2 = ((6,7), (7,6), (5,7), (6,8), (7,7))
+c3 = ((11,12), (12,11), (10,12), (11,10), (12,12))
+c4 = ((16,5), (15,6), (16,4), (17,5), (15,5))
+c5 = ((21,16), (22,15), (20,16), (21,17), (22,16))
+alumno = (11,9)
+
+
+def centro_masa(vectores):
+    sumatoria = [0,0]
+    centro = [0,0]
+    for vector in vectores :
+        sumatoria[0] += vector[0]
+        sumatoria[1] += vector[1]
+    
+    centro[0] = sumatoria[0] / len(vectores)
+    centro[1] = sumatoria[1] / len(vectores)
+    
+    return centro 
+
+
+def distancia(punto, centro):
+    dist = (((centro[0]-punto[0])**2) + ((centro[1]-punto[1])**2))**(1/2)
+    return dist
+
+
+def clasificar(alumno, *clases) :
+    distancias = []
+    for i in clases :
+        distancias.append(distancia(alumno, centro_masa(i)))
+        
+    minimo = min(distancias)
+    
+    return clases[distancias.index(minimo)]
+        
+    
+
+print(clasificar(alumno, c1, c2, c3, c4, c5))
