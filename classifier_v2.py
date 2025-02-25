b51517bb48b72597af9f8eac7982ced85ea48e4b
Daarick <e.danielgrz10@gmail.com>
2025-02-25 15:51:48 -0600

    practica 2

--- classifier_v2.py
+++ classifier_v2.py
@@ -1,41 +1,17 @@
-clases = {
-    "c1" : ((1,2), (2,3), (0,2), (1,1), (2,2)),
-    "c2" :((6,7), (7,6), (5,7), (6,8), (7,7)),
-    "c3" : ((11,12), (12,11), (10,12), (11,10), (12,12)),
-    "c4" : ((16,5), (15,6), (16,4), (17,5), (15,5)),
-    "c5" : ((21,16), (22,15), (20,16), (21,17), (22,16))
-}
+import random
+n_dimensiones = input("Ingrese el numero de dimensiones")
+n_clases = input("Ingrese el numero de clases que quiere")
+n_vectores = input("Ingrese el numero de vectores que quiere")
 
-alumno = (11,9)
-
-clases.update()
-
-
-def centro_masa(vectores):
-    sumatoria = [0,0]
-    centro = [0,0]
-    for vector in vectores :
-        sumatoria[0] += vector[0]
-        sumatoria[1] += vector[1]
+def generarClases(n_dimensiones, n_clases, n_vectores):
+    clases = []
     
-    centro[0] = sumatoria[0] / len(vectores)
-    centro[1] = sumatoria[1] / len(vectores)
-    
-    return centro 
-
-def distancia(punto, centro):
-    return (((centro[0]-punto[0])**2) + ((centro[1]-punto[1])**2))**(1/2)
-
-def clasificar(alumno, clases) :
-    distancias = []
-    for i in clases :
-        distancias.append(distancia(alumno, centro_masa(clases[i])))
-
+    for i in range(n_clases): 
+        for j in range(n_vectores):
+            for k in range(n_dimensiones):
+                clases[i][j][k].append(random.randint())
                 
-    minimo = min(distancias)
-    
-    
-    
-    return minimo
+    return clases 
 
-print(clasificar(alumno, clases))
+print(generarClases(n_dimensiones, n_clases, n_vectores))
+                
